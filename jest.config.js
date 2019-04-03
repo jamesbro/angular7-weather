e2e54f5e0ed5c0d25947db8f04ff8f8e599c5ca8
Jeremy Hull <jchull@gmail.com>
2019-04-02 18:40:31 -0600

    migrated to Jest for unit testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "preset": "jest-preset-angular",
+  "setupFilesAfterEnv": ["<rootDir>/jest-setup.ts"],
+  "verbose": true,
+  // "transform": {
+  //   "(html)": "<rootDir>/node_modules/jest-templatecache-transformer",
+  //   "(js)": "<rootDir>/test/src/jest.transform.js",
+  // },
+};
